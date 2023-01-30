b81489ad1c9b09687eb644d7f2a2404a925514f6
ryanporper <ryan_porper@aol.com>
2023-01-29 22:12:52 -0500

    implented transaction & geography pages

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -7,8 +7,8 @@ export const api = createApi({
         "User",
         "Products",
         "Customers",
-        // "Transactions",
-        // "Geography",
+        "Transactions",
+        "Geography",
         // "Sales",
         // "Admins",
         // "Performance",
@@ -27,18 +27,18 @@ export const api = createApi({
             query: () => "client/customers",
             providesTags: ["Customers"],
         }),
-        // getTransactions: build.query({
-        //     query: ({ page, pageSize, sort, search }) => ({
-        //         url: "client/transactions",
-        //         method: "GET",
-        //         params: { page, pageSize, sort, search },
-        //     }),
-        //     providesTags: ["Transactions"],
-        // }),
-        // getGeography: build.query({
-        //     query: () => "client/geography",
-        //     providesTags: ["Geography"],
-        // }),
+        getTransactions: build.query({
+            query: ({ page, pageSize, sort, search }) => ({
+                url: "client/transactions",
+                method: "GET",
+                params: { page, pageSize, sort, search },
+            }),
+            providesTags: ["Transactions"],
+        }),
+        getGeography: build.query({
+            query: () => "client/geography",
+            providesTags: ["Geography"],
+        }),
         // getSales: build.query({
         //     query: () => "sales/sales",
         //     providesTags: ["Sales"],
@@ -62,8 +62,8 @@ export const {
     useGetUserQuery,
     useGetProductsQuery,
     useGetCustomersQuery,
-    // useGetTransactionsQuery,
-    // useGetGeographyQuery,
+    useGetTransactionsQuery,
+    useGetGeographyQuery,
     // useGetSalesQuery,
     // useGetAdminsQuery,
     // useGetUserPerformanceQuery,
